d1b300a9eadeddd7f83d03fe3395957c4977845b
Maksim Alipau <maxanotest@gmail.com>
2025-02-03 16:37:34 +0300

    feat: add a task 1 from InnoDOM

---
+++ hw2/task1.js
@@ -0,0 +1,8 @@
+// Напишите функцию sayHello, которая выводит в консоль приветствие "Привет, мир!".
+
+const sayHello = (str) => {
+    console.log(str);
+}
+
+sayHello('Привет, мир!');
+sayHello('Hello world!');
