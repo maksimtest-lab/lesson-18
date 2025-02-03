5db8e49d8c5af192d1f24ce597dfc5c0f71a8511
Maksim Alipau <maxanotest@gmail.com>
2025-02-03 16:47:15 +0300

    feat: add a task 5 from InnoDOM

---
+++ hw2/task5.js
@@ -0,0 +1,9 @@
+// Напишите функцию calculateDiff, которая принимает два числа и возвращает их разность.
+
+const calculateDiff = (a, b) => {
+    return a - b;
+}
+
+console.log(calculateDiff(10, 5));
+console.log(calculateDiff(20, 10));
+console.log(calculateDiff(30, 15));
