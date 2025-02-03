dc170370720ef740b9c64527668fffd0d64aa341
Maksim Alipau <maxanotest@gmail.com>
2025-02-03 16:39:50 +0300

    feat: add a task 2 from InnoDOM

---
+++ hw2/task2.js
@@ -0,0 +1,8 @@
+// Реализуйте функцию getSquare, которая принимает число и возвращает его квадрат.
+
+const getSquare = (num) => {
+    return num ** 2;
+}
+
+console.log(getSquare(5));
+console.log(getSquare(10));
