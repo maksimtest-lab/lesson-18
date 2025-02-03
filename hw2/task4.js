bf02e3e2e917944e98ee68ddeb71f87ebe0e73f8
Maksim Alipau <maxanotest@gmail.com>
2025-02-03 16:45:17 +0300

    feat: add a task 4 from InnoDOM

---
+++ hw2/task4.js
@@ -0,0 +1,12 @@
+// Реализуйте функцию getGreeting, которая принимает имя и возвращает приветствие с использованием этого имени.
+
+const getGreeting = (name) => {
+    return `Привет, ${name}!`;
+}
+
+console.log(getGreeting('Ярослав'));
+console.log(getGreeting('Дима'));
+console.log(getGreeting('Денис'));
+console.log(getGreeting('Илья'));
+console.log(getGreeting('Аня'));
+console.log(getGreeting('Ирина'));
