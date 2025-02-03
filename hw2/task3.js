d0c35489cf59488752b2fe49257eb2913dd4884b
Maksim Alipau <maxanotest@gmail.com>
2025-02-03 16:42:27 +0300

    feat: add a task 3 from InnoDOM

---
+++ hw2/task3.js
@@ -0,0 +1,9 @@
+// Напишите функцию isEven, которая принимает число и возвращает true, если число четное, и false в противном случае.
+
+const isEven = (num) => {
+    return num % 2 === 0;
+}
+
+console.log(isEven(2));
+console.log(isEven(3));
+console.log(isEven(4));
